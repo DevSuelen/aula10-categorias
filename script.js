2f377832bcb02bf91e53da683001123065130cb8
eugenioN25111 <paivaeugenio789@gmail.com>
2023-04-12 09:47:56 -0300

    criando categorias do site

---
+++ script.js
@@ -0,0 +1,18 @@
+//CHECAR SE O LIGHTMODE ESTÁ ATIVO
+let lightMode = localStorage.getItem('lightMode');
+console.log(lightMode)
+if(lightMode === 'true'){
+    modeSwitch.classList.toggle("active");
+    topo.classList.toggle("light");
+}
+
+//AO CLICAR NO SWITCH ALTERNA ENTRE LIGHT E DARK MODE
+modeSwitch.onclick = (e) => {
+    e.target.classList.toggle("active");
+    topo.classList.toggle("light");
+    if(topo.classList.contains('light')){
+        localStorage.setItem('lightMode',true);
+        }else{
+        localStorage.setItem('lightMode',false);
+    }
+}
